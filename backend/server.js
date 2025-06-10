4f6d7cdb3f61a991364cdb031630ce44d647432f
Deepak Kumar <kumar.dee2005@gmail.com>
2025-06-10 13:21:16 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,5 @@ app.use('/api/classes', classRoutes);
 app.use('/api/students', studentRoutes);
 app.use('/api/attendance', attendanceRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
